4e646dca841bb27ae1412e9fe265edada771a367
Akshat <Supergolu123@gmail.com>
2024-02-17 21:13:53 +0530

    change routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,18 +26,18 @@ const handleLogout =()=>{
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/">Home</Link>
+              <Link className="nav-link" aria-current="page" to="/swiggy-clone">Home</Link>
             </li>
             {(localStorage.getItem("authToken")) ? <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/myOrder">My Orders</Link>
+              <Link className="nav-link" aria-current="page" to="/swiggy-clone/myOrder">My Orders</Link>
             </li>
               : ""
             }
           </ul>
           {(!localStorage.getItem("authToken")) ?
             <div className='d-flex'>
-              <Link className="btn bg-white text-primary mx-1" to="/Login">Login</Link>
-              <Link className="btn bg-white text-primary mx-1" to="/createuser">Signup</Link>
+              <Link className="btn bg-white text-primary mx-1" to="/swiggy-clone/Login">Login</Link>
+              <Link className="btn bg-white text-primary mx-1" to="/swiggy-clone/createuser">Signup</Link>
             </div>
             :
             <div>
